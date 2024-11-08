9d21250fa88a414a4b56450e21aff4948566b778
Olatunbosun Agbeyangi <a.olatubosun@centrifugegroup.com>
2024-11-08 15:36:34 +0100

    initial commit

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-function move() {}
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', event => {
+  if (event.key === 'F12' || (event.ctrlKey && event.shiftKey && event.key === 'I')) {
+    event.preventDefault();
+  }
+});
